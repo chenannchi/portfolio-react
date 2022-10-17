0ca6392c6b080225bf290a475fe3807fc4163391
chenannchi <anchichenusa@gmail.com>
2022-10-16 23:47:04 -0400

    add contact info to cotact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,10 @@
 const Contact = () => {
   return ( 
     <>
-      <h1>Contact page</h1>
+      <h2>Feel free to contact me by email or follow my GitHub account!</h2>
+      <a href = "mailto: anchichenusa@gmail.com">Email</a>
+      <a href = "https://github.com/chenannchi">GitHub</a>
+      <a href = "https://www.linkedin.com/in/anchi-chen/">Linkedin</a>
     </>
   )
 }
