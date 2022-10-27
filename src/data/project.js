6520044fa84e7bcd563e81143588e919a9503aad
chenannchi <anchichenusa@gmail.com>
2022-10-27 18:21:37 -0400

    add giftology to data

--- src/data/project.js
+++ src/data/project.js
@@ -19,5 +19,12 @@ export const projects = [
     image:"https://github.com/chenannchi/ttt-weekend/raw/main/tic-tac-toe.png",
     repositoryLink:"https://github.com/chenannchi/ttt-weekend",
     deploymentLink:"https://chenannchi-ttt.netlify.app/"
-  }   
+  },
+  {
+    title:"Giftology",
+    description:"Our goal for our application (Giftology) is to reduce the number of hours trying to find the perfect gift for your loved ones. Giftology allows you to create your wishlist and let your friends view your wishlists. It allows users to improve their gift-giving experience by facilitating the communication between gift-givers and receivers. Also, allow for reducing resource waste generated from gifting gone wrong.",
+    image:"",
+    repositoryLink:"https://github.com/creyes25/giftology-frontend",
+    deploymentLink:"https://giftology-madeez.netlify.app/"
+  },
 ]
