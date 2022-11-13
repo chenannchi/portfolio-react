eeb704be6d8b066050fc6257207ac99de6bfe2fb
chenannchi <anchichenusa@gmail.com>
2022-11-13 00:18:36 -0500

    modify the layout of projects page in larger screen

--- src/components/Projects/ProjectsList.jsx
+++ src/components/Projects/ProjectsList.jsx
@@ -1,14 +1,15 @@
 import ProjectPreview from "./ProjectPreview";
+import styles from "../Projects/Projects.module.css"
 
 const ProjectsList = (props) => {
   return ( 
-    <>
+    <div className={styles.cardsContainer}>
       {props.projects.map((project, idx) => (
         // <li key={idx}>
           <ProjectPreview key={idx} project={project} />
         // </li>
       ))}
-    </>
+    </div>
   );
 }
  
