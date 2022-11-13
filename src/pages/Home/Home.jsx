b4c2caacfed5d02013d46891ff812ab6283e48c4
chenannchi <anchichenusa@gmail.com>
2022-11-12 23:59:26 -0500

    modify the layout of home page in larger screen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,17 +5,18 @@ const Home = () => {
   return (
     <main>
       <div className={styles.homeImage}>
-        <div className={styles.titleContainer}>
-          <div id={styles.welcome}>Welcome to my portfolio!</div>
-        </div>
         <img className={styles.profile} src="./images/photo.jpg" alt="profile" />
-        <div className={styles.titleContainer}>
-          <div id={styles.name}>Anchi Chen</div>
+        <div>
+          <div className={styles.titleContainer}>
+            <div id={styles.welcome}>Welcome to my portfolio!</div>
+          </div>
+          <div className={styles.titleContainer}>
+            <div id={styles.name}>I'm Anchi Chen.</div>
+          </div>
+          <div className={styles.titleContainer}>
+            <div id={styles.title}>I'm a Software Engineer.</div>
+          </div>
         </div>
-        <div className={styles.titleContainer}>
-          <div id={styles.title}>Software Engineer</div>
-        </div>
-
       </div>
 
       {/* <About /> */}
