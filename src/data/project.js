2bf807f4fc6c8c7c1e660b0dd5f476c79a62bb5d
chenannchi <anchichenusa@gmail.com>
2022-12-14 02:29:43 +0800

    add description to connect four

--- src/data/project.js
+++ src/data/project.js
@@ -1,14 +1,14 @@
 export const projects = [
   {
     title:"Playlist Collector",
-    description:"Collect your favorite songs by creating some playlists!Share your idea by leaving comments with others below each playlist!Finally, let's heal together by listening to music!",
+    description:"Collect your favorite songs by creating some playlists! Share your idea by leaving comments with others below each playlist! Finally, let's heal together by listening to music!",
     image:"../../images/playlist_collector.png",
     repositoryLink:"https://github.com/chenannchi/playlist-collector",
     deploymentLink:"https://healing-playlist-collector.herokuapp.com/"
   },
   {
     title:"Giftology",
-    description:"Our goal for our application (Giftology) is to reduce the number of hours trying to find the perfect gift for your loved ones. Giftology allows you to create your wishlist and let your friends view your wishlists. It allows users to improve their gift-giving experience by facilitating the communication between gift-givers and receivers. Also, allow for reducing resource waste generated from gifting gone wrong.",
+    description:"Our goal for our application (Giftology) is to reduce the number of hours trying to find the perfect gift for your loved ones. Giftology allows you to create your wishlist and let your friends view your wishlists. Also, it allows users to improve their gift-giving experience by facilitating the communication between gift-givers and receivers. Also, allow for reducing resource waste generated from gifting gone wrong.",
     image:"../../images/giftology.png",
     repositoryLink:"https://github.com/creyes25/giftology-frontend",
     deploymentLink:"https://giftology-madeez.netlify.app/"
@@ -22,7 +22,7 @@ export const projects = [
   }, 
   {
     title:"Connect Four",
-    description:"Connect four is the first project that I built in General Assembly. According to Wikipedia, Connect Four is a two-player connection board game, in which the players choose a color and then take turns dropping colored tokens into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own tokens.",
+    description:"Connect Four is my first project I built during my time in General Assembly. I implemented some animations on the tokens and message. Also, I add a background music and sound effect to the game. The functionalites which I like the most is the hover effect that I add on the columns of the game board.",
     image:"../../images/connect4.png",
     repositoryLink:"https://github.com/chenannchi/Connect-Four",
     deploymentLink:"https://anchi-chen-connect-four.netlify.app/game.html?"
