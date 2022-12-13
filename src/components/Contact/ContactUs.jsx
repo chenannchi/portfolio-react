e656c979b0d39f519e91ae9ffb6672694bc28811
chenannchi <anchichenusa@gmail.com>
2022-12-13 22:34:30 +0800

    change the UI after submit the email form

--- src/components/Contact/ContactUs.jsx
+++ src/components/Contact/ContactUs.jsx
@@ -1,14 +1,17 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from "emailjs-com"
 import styles from "../Contact/ContactUs.module.css"
 
 
 export const ContactUs = () => {
   const form = useRef();
+  const [submit, setSubmit] = useState(false)
+
+  console.log(submit)
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    setSubmit(!submit)
     emailjs.sendForm(
       process.env.REACT_APP_SERVICE_ID,
       process.env.REACT_APP_TEMPLATE_ID,
@@ -23,20 +26,27 @@ export const ContactUs = () => {
 
   return (
     <div>
-      <h2>
-        Send me an email!
-      </h2>
-      <form ref={form} onSubmit={sendEmail} className={styles.form} autoComplete="off">
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <div className={styles.submitBtn} >
-          <input type="submit" value="Send" className={styles.btn} />
-        </div>
-      </form>
+      {submit?
+      <p id={styles.statusMsg}>Your email has been sent successfully!</p>
+      :
+      <div>
+        <h2>
+          Send me an email!
+        </h2>
+        <form ref={form} onSubmit={sendEmail} className={styles.form} autoComplete="off">
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <div className={styles.submitBtn} >
+            <input type="submit" value="Send" className={styles.btn} />
+          </div>
+        </form>
+      </div>
+      
+      }
     </div>
   );
 };
