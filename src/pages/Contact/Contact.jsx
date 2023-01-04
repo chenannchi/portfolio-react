b6b58f4b3bcc69b4c9e12b643dd33a02791bcc9c
chenannchi <anchichenusa@gmail.com>
2023-01-05 01:35:30 +0800

    add target attribute to link of contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -12,13 +12,13 @@ const Contact = () => {
         <div className={styles.contactInfo}>
           <h2>Feel free to contact & follow me!</h2>
           <div className={styles.icons}>
-            <a href="mailto: anchichenusa@gmail.com">
+            <a href="mailto: anchichenusa@gmail.com" target="_blank" rel="noreferrer">
               <img src="https://cdn-icons-png.flaticon.com/128/8760/8760637.png" alt="email icon" />
             </a>
-            <a href="https://github.com/chenannchi">
+            <a href="https://github.com/chenannchi" target="_blank" rel="noreferrer">
               <img src="https://cdn-icons-png.flaticon.com/128/3488/3488426.png" alt="github icon" />
             </a>
-            <a href="https://www.linkedin.com/in/anchi-chen/">
+            <a href="https://www.linkedin.com/in/anchi-chen/" target="_blank" rel="noreferrer">
               <img src="https://cdn-icons-png.flaticon.com/128/3488/3488326.png" alt="linkedin icon" />
             </a>
           </div>
