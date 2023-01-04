506cd0b6db7aa749b4ec55fc73788019a393366b
chenannchi <anchichenusa@gmail.com>
2023-01-05 00:16:35 +0800

    adjust the width of brand statement on about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,14 +8,14 @@ const About = () => {
       </div>
       <div className={styles.brandStatement}>
         <p>
-        As a software engineer with a background in healthcare administration and computer science, I bring a detail-oriented and self-motivated mindset to what I build. 
-        <br />
-        <br />
-        I utilize creative problem-solving, and cross-functionally collaborating skills as well as multiple software development tools such as HTML, CSS, JS, and MongoDB to create valuable applications. 
-        <br />
-        <br />
-        I am dedicated to designing and building websites to improve the way users access information.
-        <br />
+          As a software engineer with a background in healthcare administration and computer science, I bring a detail-oriented and self-motivated mindset to what I build. 
+          <br />
+          <br />
+          I utilize creative problem-solving, and cross-functionally collaborating skills as well as multiple software development tools such as HTML, CSS, JS, and MongoDB to create valuable applications. 
+          <br />
+          <br />
+          I am dedicated to designing and building websites to improve the way users access information.
+          <br />
         </p>
       </div>
     </main>
