1e71ca1487532f81b790ff80d53a2b9fd572e643
chenannchi <anchichenusa@gmail.com>
2023-03-19 15:25:42 -0400

    modify the layout of about page and replace the image with another image

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,22 +1,22 @@
 import styles from "../About/About.module.css"
 
 const About = () => {
-  return ( 
+  return (
     <main className={styles.pageContainer}>
-      <div className={styles.aboutImg}>
-        <img className={styles.profileImage} src="./images/photo.jpg" alt="profile" />
-      </div>
-      <div className={styles.brandStatement}>
-        <p>
-          As a software engineer with a background in healthcare administration and computer science, I bring a detail-oriented and self-motivated mindset to what I build. 
-          <br />
-          <br />
-          I utilize creative problem-solving, and cross-functionally collaborating skills as well as multiple software development tools such as HTML, CSS, JS, and MongoDB to create valuable applications. 
-          <br />
-          <br />
-          I am dedicated to designing and building websites to improve the way users access information.
-          <br />
-        </p>
+      <div className={styles.section}>
+        <div className={styles.brandStatement}>
+          <p>
+            As a software engineer with a background in healthcare administration and computer science, I bring a detail-oriented and self-motivated mindset to what I build.
+            <br />
+            <br />
+            I utilize creative problem-solving, and cross-functionally collaborating skills as well as multiple software development tools such as HTML, CSS, JS, and MongoDB to create valuable applications.
+            <br />
+            <br />
+            I am dedicated to designing and building websites to improve the way users access information.
+            <br />
+          </p>
+        </div>
+        <img className={styles.profileImage} src="./images/photo2.jpg" alt="profile" />
       </div>
     </main>
   )
