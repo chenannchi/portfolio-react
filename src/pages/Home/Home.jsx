364beab69ef0cb929beb0f864dfee7e6e13f82dd
chenannchi <anchichenusa@gmail.com>
2023-03-20 13:57:41 -0400

    replace my photo on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <main>
       <div className={styles.homeImage}>
-        <img className={styles.profile} src="./images/photo.jpg" alt="profile" />
+        <img className={styles.profile} src="./images/photo2.jpg" alt="profile" />
         <div>
           <div className={styles.titleContainer}>
             <div id={styles.welcome}>Welcome to my portfolio!</div>
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,11 +1,12 @@
 import styles from "../Resume/Resume.module.css"
 
 const Resume = () => {
+
   return (
     <main className={styles.pageContainer}>
       <div>
         {/* <a href="Anchi Chen Resume.pdf" download>Resume</a> */}
-        <iframe src="Anchi-Chen-Resume.pdf" title="resume" id={styles.resume}
+        <iframe src="Anchi-Chen-Resume.pdf" title="resume" id={styles.resume} 
         ></iframe>
       </div>
     </main>
