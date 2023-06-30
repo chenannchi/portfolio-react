c139742134bfeafbc8b24e3a4277acd97557478e
chenannchi <anchichenusa@gmail.com>
2023-06-30 23:40:25 +0800

    add mandarin version of content on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,24 @@
 import styles from "../Home/Home.module.css"
 
-const Home = () => {
+const Home = ({ handleLanguageChange, isLanguageEng }) => {
   return (
     <main>
       <div className={styles.homeImage}>
         <img className={styles.profile} src="./images/photo2.jpg" alt="profile" />
         <div>
           <div className={styles.titleContainer}>
-            <div id={styles.welcome}>Welcome to my portfolio!</div>
+            <div id={styles.welcome}>{isLanguageEng ? "Welcome to my portfolio!" : "歡迎來到我的作品集"}</div>
           </div>
           <div className={styles.titleContainer}>
-            <div id={styles.name}>I'm&nbsp;<b>Anchi Chen</b>.</div>
+            <div id={styles.name}>
+              {isLanguageEng ?
+                "I'm Anchi Chen." : "我是陳安琪!"
+              }
+            </div>
           </div>
           <div className={styles.titleContainer}>
-            <div id={styles.title}>---- A Full-Stack Software Engineer!</div>
+            <div id={styles.title}>{isLanguageEng ? "---- A Full-Stack Software Engineer!" : "一位全端軟體工程師"}
+            </div>
           </div>
         </div>
       </div>
